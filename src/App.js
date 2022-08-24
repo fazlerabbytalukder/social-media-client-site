81fdcdc38f80629afe6ae0be031007df51c0d2af
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-08-23 23:47:30 -0700

    create component and style it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import logo from './images/social-site-logo.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Forms/Form';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Social Media</Typography>
+                <img className={classes.image} src={logo} alt="logo" height="60" />
+            </AppBar>
+
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" specing={4}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts></Posts>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form></Form>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 };
 
